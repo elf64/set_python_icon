0a8e1b834684982d0c0df98a1264f5153ad30a72
PukaPy <pukalordu@yahoo.com>
2017-07-26 21:08:47 +0300

    Update ico.py

--- ico.py
+++ ico.py
@@ -1,32 +1,65 @@
 import os
+import sys
 import pip
-import urllib
+import requests
 from _winreg import *
-# Install Pillow
-try:
-	from PIL import Image
-except:
-	print "Pillow not found!"
-	print "Installing Pillow"
-	pip.main(['install', 'Pillow'])
-# Download the image
-png_to_ico = urllib.URLopener()
-png_to_ico.retrieve("https://s24.postimg.org/xngee8e3p/page.png",
-					"page.png")
-# Create folder for .ico
-if not os.path.exists('C:\\pyico'):
-	os.makedirs('C:\\pyico')
-# Convert .png to .ico
-original = Image.open('page.png')
-original.save('C:\\pyico\\python.ico')
-# Set the registry sub-key to the new icon
-keyVal = 'Python.File\\DefaultIcon'
-key = OpenKey(HKEY_CLASSES_ROOT, keyVal, 0, KEY_ALL_ACCESS)
-SetValueEx(key, "", 0, REG_SZ, "C:\pyico\python.ico")
-CloseKey(key)
-# Remove the page.png file
-os.remove("page.png")
-# Creating setico.bat which will refresh the icons
-with open('setico.bat', 'w') as f:
-	f.write('ie4uinit.exe -show\nexit')
-print "Run setico.bat to refresh the icons!!!"
+
+
+def download():
+    with open("page.png", "wb") as f:
+        print "Downloading the Icon from {}".format(link)
+        r = requests.get(
+            link, stream=True
+        )
+        t_l = r.headers.get('content-length')
+        if t_l is None:
+            f.write(r.content)
+        else:
+            dl = 0
+            t_l = int(t_l)
+            for d in r.iter_content(chunk_size=4096):
+                dl += len(d)
+                f.write(d)
+                done = int(50 * dl / t_l)
+                sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50 - done)))
+                sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    # Install Pillow
+    try:
+        from PIL import Image
+    except:
+        print "Pillow not found!"
+        print "Installing Pillow"
+        pip.main(['install', 'Pillow'])
+
+    link = "https://s24.postimg.org/xngee8e3p/page.png"
+    if not os.path.exists('C:\\pyico'):
+        os.makedirs('C:\\pyico')
+
+    # Convert .png to .ico
+
+    download()
+    original = Image.open('page.png')
+    original.save('C:\\pyico\\python.ico')
+    os.remove("page.png")
+
+    # Set the registry sub-key to the new icon
+
+    keyVal = 'Python.File\\DefaultIcon'
+    key = OpenKey(HKEY_CLASSES_ROOT, keyVal, 0, KEY_ALL_ACCESS)
+    SetValueEx(key, "", 0, REG_SZ, "C:\\pyico\\python.ico")
+    CloseKey(key)
+
+    # Creating setico.bat which will refresh the icons
+
+    with open('setico.bat', 'wb') as f:
+        f.write("""
+        @ECHO OFF
+        SETLOCAL
+        ie4uinit.exe -show
+        DEL "%~f0"
+        """)
+
+    print "\nRun setico.bat to refresh the icons!!!"
